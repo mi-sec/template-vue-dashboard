e773160952a4b39c3b6ae1ad8621599c1c66e2c8
iskore <nicksoggin@gmail.com>
2020-01-22 19:22:05 -0500

    edit build phase to account for gh-pages path

--- vue.config.js
+++ vue.config.js
@@ -4,9 +4,19 @@ process.env.VUE_APP_TITLE       = pack.name;
 process.env.VUE_APP_DESCRIPTION = pack.description;
 process.env.VUE_APP_VERSION     = pack.version;
 
-module.exports = {
+const config = {
 	productionSourceMap: process.env.NODE_ENV !== 'production',
 	transpileDependencies: [
 		'vuetify'
 	]
 };
+
+process.env.GITHUB_ACTIONS    = 'true';
+process.env.GITHUB_REPOSITORY = 'mi-sec/template-vue-dashboard';
+
+console.log( process.env.GITHUB_ACTIONS );
+if ( process.env.GITHUB_ACTIONS === 'true' ) {
+	config.publicPath = `/${ process.env.GITHUB_REPOSITORY.split( '/' ).pop().trim() }/`;
+}
+
+module.exports = config;
