52f9a7a6ee4d6f2a9de0bf860b0b9c8d07f5a1f9
iskore <nicksoggin@gmail.com>
2020-01-22 20:29:01 -0500

    added time and version to footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -10,8 +10,9 @@
 		<v-spacer></v-spacer>
 
 		<v-btn
-			height="26"
-			width="26"
+			:height="footer.height"
+			:width="footer.height"
+			class="mr-5"
 			tile
 			icon
 			x-small
@@ -20,16 +21,44 @@
 		>
 			<v-icon>mdi-invert-colors</v-icon>
 		</v-btn>
+
+		<v-btn
+			:height="footer.height"
+			:width="footer.height"
+			class="mr-5"
+			tile
+			x-small
+			aria-label="app-version"
+			:href="repoUrl"
+		>
+			{{ version }}
+		</v-btn>
+
+		<span class="overline">{{ now }}</span>
 	</v-footer>
 </template>
 
 <script>
 	import { mapState } from 'vuex';
+	import moment       from 'moment';
 
 	export default {
 		name: 'Footer',
 		data() {
-			return {};
+			return {
+				version: `v${ process.env.VUE_APP_VERSION }`,
+				repoUrl: process.env.VUE_APP_REPOSITORY || '/',
+				now: moment().format( 'D MMM YYYY, HH:mm:ss' )
+			};
+		},
+		mounted() {
+			this.timeInterval = setInterval(
+				() => this.now = moment().format( 'D MMM YYYY, HH:mm:ss' ),
+				60
+			);
+		},
+		beforeDestroy() {
+			clearInterval( this.timeInterval );
 		},
 		computed: {
 			...mapState( [ 'footer' ] )
